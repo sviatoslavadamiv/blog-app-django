83c32596684452ba9f49a3d9c826040771029f25
Sviatoslav Adamiv <sviatoslav@MacBook-Pro-Sviatoslav.local>
2025-07-20 18:14:52 +0200

    Add get_absolute_url() method to Post model and replace post detail URLs in templates

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse_lazy
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -40,3 +41,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy(
+            'blog:post_detail',
+            args=[self.id],
+        )
