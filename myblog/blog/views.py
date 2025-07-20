3bebb6e242fa7f43d425dce0e3fb1bc6d6d660be
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-20 21:21:18 +0200

    Pagination added and views functions were rewritten to class-based views

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,27 +1,64 @@
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
-def post_list(request) -> HttpResponse:
-    posts = Post.published.all()
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts},
-    )
+# def post_list(request) -> HttpResponse:
+#     post_list = Post.published.all()
+#
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts},
+#     )
 
-def post_detail(request, year, month, day, post) -> HttpResponse:
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day,
-    )
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post},
-    )
+class PostDetailView(DetailView):
+    queryset = Post.published.all()
+    template_name = 'blog/post/detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        queryset = self.get_queryset()
+        return get_object_or_404(
+            queryset,
+            slug=self.kwargs.get('post'),
+            publish__year=self.kwargs.get('year'),
+            publish__month=self.kwargs.get('month'),
+            publish__day=self.kwargs.get('day'),
+        )
+
+
+# def post_detail(request, year, month, day, post) -> HttpResponse:
+#     post = get_object_or_404(
+#         Post,
+#         status=Post.Status.PUBLISHED,
+#         slug=post,
+#         publish__year=year,
+#         publish__month=month,
+#         publish__day=day,
+#     )
+#     return render(
+#         request,
+#         'blog/post/detail.html',
+#         {'post': post},
+#     )
